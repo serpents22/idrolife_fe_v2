69cbf7d19d872b8bd0d18b2b545aff13dad04943
serpents22 <ivandarmawan67@gmail.com>
2024-06-04 21:42:44 +0700

    fix: handle undefined data

--- src/views/Report/EventiProgrammi.vue
+++ src/views/Report/EventiProgrammi.vue
@@ -269,9 +269,9 @@ function fillTableData() {
         if (tmpMasterStatus.charAt(7) === '1') { masterStatus.value.push(true) } else { masterStatus.value.push(false) }
         // console.log(masterStatus.value)
       }
-      
+
       // if (data.hasOwnProperty('S85')) {
-        if (data.S86 !== undefined) {
+        if (data.S85 !== undefined) {
         console.log(data.S85)
         tmpRestOfContatore = data.S85.split(',')
       } else {
